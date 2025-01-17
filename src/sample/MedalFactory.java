c7a1e517a8d8d15e44fa858fc2924ca23f21fa12
Toida <tetsuro.toida@rakus-partners.co.jp>
2025-01-17 17:27:30 +0900

    update

--- src/sample/MedalFactory.java
+++ src/sample/MedalFactory.java
@@ -3,8 +3,7 @@ package sample;
 public class MedalFactory {
 
     
-    int totalSalary=0;
-    
+    private double totalSalary=0
 
     for(Employee employees: employee){
     totalSalary=totalSalary+employees.getSalary;
